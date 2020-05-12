6669ffdf576e24f0866e5919e621c93aa9a9f8a3
petr0v <slowpooqa@gmail.com>
2020-05-12 15:32:03 -0500

    Upgrade task

--- 04_data_structures/task_4_5.py
+++ 04_data_structures/task_4_5.py
@@ -16,20 +16,13 @@ command2 = 'switchport trunk allowed vlan 1,3,8,9'
 
 command1 = command1.split()
 command1 = command1[-1].split(',')
-
 command2 = command2.split()
 command2 = command2[-1].split(',')
-
-result = command1 + command2
+command1 = set(command1)
+command2 = set(command2)
+result = command1 & command2
 result = sorted(result)
-
-result.remove('1')                                                    
-result.remove('2')                                                    
-result.remove('3')                                                    
-result.remove('5')                                                    
-result.remove('8')                                                    
-result.remove('9')                                                    
-
+                       
 print(result)
 
 
