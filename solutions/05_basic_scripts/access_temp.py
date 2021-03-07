cd4e26abf5c3368caa5d5b8fbeaa744536ccb213
petr0v <slowpooqa@gmail.com>
2021-03-07 08:10:58 -0600

    Сделал задание 5.1

---
+++ solutions/05_basic_scripts/access_temp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+vlan = sys.argv[1]
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('\n'.join(access_template).format(vlan))
